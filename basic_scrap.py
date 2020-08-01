d3c40266952b20f4984133ed8f92f2537faa68e1
Samuel Rudnicki <srudnicki@inf.ufrgs.br>
2020-08-01 17:34:22 -0300

    Completed initial search

--- basic_scrap.py
+++ basic_scrap.py
@@ -27,12 +27,27 @@ class MercadoLivreAPI:
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
         self.driver = get_chrome_web_driver(options)
-        
-        time.sleep(1)
+    
+
+    def run(self):
+        print("[*] STARTING [*]")
+        print(f"Looking for {self.product_name}...")
+        links = self.get_products_links()
+        time.sleep(2)
         self.driver.quit()
+        pass
+    
+    def get_products_links(self):
+        self.driver.get(self.base_url)
+        element = self.driver.find_element_by_xpath("//input[contains(@class, 'nav-search-input')]")
+        element.send_keys(self.product_name)
+        element.send_keys(Keys.ENTER)
+        time.sleep(2)
+        pass
+
 
 if __name__ == '__main__':
     ml = MercadoLivreAPI(NAME, FILTERS, BASE_URL, CURRENCY)
-    
+    data = ml.run()
 
 
